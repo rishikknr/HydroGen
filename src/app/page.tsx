6f04c5ced49dcb0d0e1b67f6c78c48147f6c63ca
Rishik Kothapally <rishikkothapally@gmail.com>
2025-06-15 14:45:07 +0000

    i can see nothing in screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Navbar from "@/components/layout/navbar";
+import HeroSection from "@/components/sections/hero-section";
+import Footer from "@/components/layout/footer";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col min-h-screen bg-background">
+      <Navbar />
+      <main className="flex-grow">
+        <HeroSection />
+        {/* Other sections will go here */}
+      </main>
+      <Footer />
+    </div>
+  );
 }
